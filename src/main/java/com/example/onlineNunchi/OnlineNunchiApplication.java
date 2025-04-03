a71d0d41b8ab98e4b5bb068e6aa05048df89f2a9
DangerousLittleBear <youngbin0817@gmail.com>
2025-04-03 21:25:09 +0900

    first commit

---
+++ src/main/java/com/example/onlineNunchi/OnlineNunchiApplication.java
@@ -0,0 +1,13 @@
+package com.example.onlineNunchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineNunchiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineNunchiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/onlineNunchi/OnlineNunchiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineNunchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineNunchiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
